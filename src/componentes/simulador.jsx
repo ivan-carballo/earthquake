c8110689ef311a5c7315126ea858ec00dd262a60
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-06-05 17:26:19 +0200

    Crear form base para la simulacion

--- src/componentes/simulador.jsx
+++ src/componentes/simulador.jsx
@@ -10,7 +10,25 @@ function Simulador() {
 
     return (
         <div>
-            <h1>Estas en el simulador de terremotos</h1>
+            <div id='rotulo_p'>
+                <h1>Estas en el simulador de terremotos</h1>
+            </div>
+
+            <div id='form_sim'>
+                <form>
+                    <label htmlFor="mag">Magnitud: </label>
+                    <select name='mag'>
+                        <option selected>0</option>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                    </select>
+                    <br />
+                    <input id='button_sim' name='sim' type='button' value='Comenzar simulacion' />
+                </form>
+            </div>
         </div>
     )
 }
