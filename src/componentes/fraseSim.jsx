7956a913ddf7c2e1065be36f68d3e33e2c5bda0a
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-06-06 13:57:24 +0200

    Crear arrays externos con texto necesario, nuevas img y estructura html y css

---
+++ src/componentes/fraseSim.jsx
@@ -0,0 +1,21 @@
+
+const frase = [
+    '¿En serio has querido simular un seismo con cero de magnitud?',
+    '¿Eso ha sido un seismo o la tierra queriendome hacer cosquillas?',
+    'Esto comienza a ser incomodo, no es como al derribar una torre de naipes',
+    '¿Todos esos gritos son por tu culpa? Nah, seguro que no has tenido nada que ver',
+    'Te has venido muy arriba, justo lo contrario que los edificios',
+    'A ti te gusta ver el mundo arder ... literalmente'
+]
+
+
+const reset = [
+    'Reparando todo lo que has roto',
+    '../../public/bola_wait.gif'
+]
+
+
+export {
+    frase,
+    reset
+}
--- src/componentes/simulador.jsx
+++ src/componentes/simulador.jsx
@@ -1,29 +1,50 @@
 import { useState, useRef, useEffect } from 'react'
 
 import '../css/simulador.css'
+import { frase, reset } from './fraseSim'
 
 
 
 
 
 function Simulador() {
+    const [antes, setAntes] = useState('')
+    const [despues, setDespues] = useState('')
+    const [meme, setMeme] = useState('')
+    const [text, setText] = useState('')
+    const [reinicio, setReinicio] = useState('')
 
 
 
     async function iniciarSim(e) {
         const magSelected = e.target.form[0].value
-
         let i = 0
+
+        setAntes()        
+
         const interval = setInterval(() => {
-
-            // Repeticion
-
+            
             i++;
             if (i >= 10) {
               clearInterval(interval);
-              // Resultados
+              setDespues()
+              setText(frase[magSelected])
+              setMeme()
             }
           }, 500);
+    }
+
+
+    async function reiniciar() {
+        setAntes('')
+        setDespues('')
+        setMeme('')
+        setText('')
+        setReinicio(reset)
+        
+        setTimeout(() => {
+            setReinicio('')
+        }, 2500);
     }
 
 
@@ -31,7 +52,7 @@ function Simulador() {
     return (
         <div>
             <div id='rotulo_p'>
-                <h1>Estas en el simulador de terremotos</h1>
+                <h1>Estas en el simulador de seismos</h1>
             </div>
 
             <div id='form_sim'>
@@ -47,11 +68,21 @@ function Simulador() {
                     </select>
                     <br />
                     <input id='button_sim' name='sim' type='button' value='Comenzar simulacion' onClick={iniciarSim}/>
+                    <br />
+                    <input id='button_sim' name='sim' type='button' value='Reiniciar' onClick={reiniciar}/>
                 </form>
             </div>
 
             <div id='estructura'>
-
+                <div id='mensaje'>
+                    <h3>{text}</h3>
+                    <h3>{reinicio[0]}</h3>
+                    <img src={reinicio[1]} />
+                </div>
+                <div id='img'>
+                    <img src={antes} />
+                    <img src={despues} />
+                </div>
             </div>
         </div>
     )
